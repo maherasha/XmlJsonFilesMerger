6a450975ee2e125128a675e37acfb2b99c2b8f71
Maher Asha <maherasha1@gmail.com>
2022-10-25 09:44:49 +0300

    added more description to FreshcellsTaskApplication.java

--- src/main/java/com/freshcells/freshcellstask/FreshcellsTaskApplication.java
+++ src/main/java/com/freshcells/freshcellstask/FreshcellsTaskApplication.java
@@ -2,19 +2,21 @@ package com.freshcells.freshcellstask;
 
 import com.freshcells.freshcellstask.service.HotelsService;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-
+/**
+ * the file will execute the merging process
+ */
 public class FreshcellsTaskApplication {
+	public static final HotelsService hotelsService = new HotelsService();
 
+	/**
+	 * The methods will execute the merging process for the hotel data files into one json file, to update the dir and json file location
+	 * please change the attribute values in the application.properties file (JSON_OUT_FILE_PATH and HOTEL_XML_FILES_DIRECTORY)
+	 */
 	public static void consolidateHotelsData(){
-		HotelsService hotelsService =  new HotelsService();
 		hotelsService.consolidateHotelsData();
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		FreshcellsTaskApplication.consolidateHotelsData();
 	}
 
